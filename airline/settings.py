e1bb7830dd276ae7006ea129434284d984abecbd
Noman Patel <Nomanpatel39424@gmail.com>
2023-05-11 18:39:14 -0400

    first commit

--- airline/settings.py
+++ airline/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#c=615y)(e1#of65x8^rloce@z4u6mos9s74+poz82pju847k8'
+SECRET_KEY = 'XXXXXXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
